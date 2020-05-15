4e41d30d7585240fe076e02168fcf1c4ee9774d0
cjferm16 <cjferm16@gmail.com>
2020-05-15 12:16:48 -0700

    tic tac toe final updates and one exercise

--- Lesson 6/tictactoe.rb
+++ Lesson 6/tictactoe.rb
@@ -1,18 +1,17 @@
-require 'pry'
-
 WINNING_LINES =
   [[1, 2, 3], [4, 5, 6], [7, 8, 9]] +
   [[1, 4, 7], [2, 5, 8], [3, 6, 9]] +
   [[1, 5, 9], [3, 5, 7]]
 
 INITIAL_MARKER = ' '
-PLAYER_MARKER = 'X'
-COMPUTER_MARKER = 'O'
+PLAYER_MARKER = "\u{2716}"
+COMPUTER_MARKER = "\u{00A9}"
+VALID_ANSWER = %w(y yes n no)
 
-# rubocop:disable Metrics/AbcSize, Metrics/MethodLength:
-def display_board(brd, scre)
+# rubocop:disable Metrics/AbcSize:
+def display_board(brd)
   system 'clear'
-  puts "You're X, Computer is O"
+  puts "You're \u{2716}, Computer is \u{00A9}"
   puts ""
   puts "     |     |"
   puts "  #{brd[1]}  |  #{brd[2]}  |  #{brd[3]}"
@@ -27,12 +26,27 @@ def display_board(brd, scre)
   puts "     |     |"
   puts ""
 end
-# rubocop:enable Metrics/AbcSize, Metrics/MethodLength:
+# rubocop:enable Metrics/AbcSize:
 
 def initialize_board
   new_board = {}
   (1..9).each { |num| new_board[num] = INITIAL_MARKER }
   new_board
+end
+
+def welcome
+  prompt "\u{2716} Welcome to Tic Tac Toe! \u{00A9}"
+  prompt "First to 5 wins!"
+  prompt "Would you like to go first?"
+  prompt "This will determine who goes first until a winner is declared!"
+  prompt "Please enter a response:"
+end
+
+def display_score(array)
+  prompt "The score is:"
+  prompt "Player: #{array[0]}"
+  prompt "Computer: #{array[1]}"
+  prompt "Ties: #{array[2]}"
 end
 
 def empty_squares(brd)
@@ -59,12 +73,12 @@ def computer_defense(setup)
   WINNING_LINES.each do |line|
     if setup.values_at(*line).count(PLAYER_MARKER) == 2 &&
        setup.values_at(*line).count(INITIAL_MARKER) == 1
-      square = 
-        (line.select { |value| value if setup[value] == INITIAL_MARKER})
+      square =
+        (line.select { |value| value if setup[value] == INITIAL_MARKER })
       square = square[0]
     end
   end
- square
+  square
 end
 
 def computer_offense(setup)
@@ -72,27 +86,71 @@ def computer_offense(setup)
   WINNING_LINES.each do |line|
     if setup.values_at(*line).count(COMPUTER_MARKER) == 2 &&
        setup.values_at(*line).count(INITIAL_MARKER) == 1
-      square = 
-        (line.select { |value| value if setup[value] == INITIAL_MARKER})
+      square =
+        (line.select { |value| value if setup[value] == INITIAL_MARKER })
       square = square[0]
     end
   end
- square
+  square
+end
+
+def choose_who_goes_first(choice)
+  if choice == 'y' || choice == 'yes'
+    true
+  else
+    false
+  end
+end
+
+def who_goes_first(choice, brd, scre)
+  if choice
+    player_goes_first(brd, scre)
+  else
+    computer_goes_first(brd, scre)
+  end
+end
+
+def player_goes_first(board, score)
+  loop do
+    display_board(board)
+    display_score(score)
+
+    player_places_piece!(board)
+    break if someone_won?(board) || board_full?(board)
+
+    computer_places_piece!(board)
+    break if someone_won?(board) || board_full?(board)
+  end
+end
+
+def computer_goes_first(board, score)
+  loop do
+    computer_places_piece!(board)
+    break if someone_won?(board) || board_full?(board)
+    display_board(board)
+    display_score(score)
+
+    player_places_piece!(board)
+    break if someone_won?(board) || board_full?(board)
+  end
+end
+
+def computer_pick_square_5(brd)
+  5 if brd[5] == INITIAL_MARKER
 end
 
 def computer_random_pick(brd)
   empty_squares(brd).sample
 end
 
-
 def computer_places_piece!(brd)
-  sqr = 
-  computer_offense(brd) ||
-  computer_defense(brd) ||
-  computer_random_pick(brd)
+  sqr =
+    computer_offense(brd) ||
+    computer_defense(brd) ||
+    computer_pick_square_5(brd) ||
+    computer_random_pick(brd)
   brd[sqr] = COMPUTER_MARKER
 end
-
 
 def board_full?(brd)
   empty_squares(brd).empty?
@@ -130,22 +188,26 @@ def joinor(array, joiner = ', ', word = 'or')
 end
 
 #------------------------------------------------------------#
+
 loop do
-  score = [0, 0]
+  score = [0, 0, 0]
+  response = ''
+  system 'clear'
+  welcome
+
+  loop do
+    response = gets.chomp.downcase
+    if VALID_ANSWER.include?(response)
+      break
+    else
+      prompt "Invalid choice. Try 'y, yes, n, or no'"
+    end
+  end
 
   loop do
     board = initialize_board
 
-    loop do
-      display_board(board, score)
-
-      player_places_piece!(board)
-      break if someone_won?(board) || board_full?(board)
-
-      computer_places_piece!(board)
-      break if someone_won?(board) || board_full?(board)
-    end
-    display_board(board, score)
+    who_goes_first(choose_who_goes_first(response), board, score)
 
     if someone_won?(board)
       prompt "#{detect_winner(board)} won!"
@@ -155,15 +217,16 @@ loop do
         score[1] += 1
       end
     else
+      score[2] += 1
       prompt "It's a tie!"
     end
     break if score[0] == 5 || score[1] == 5
   end
-    prompt "Player: #{score[0]} - Computer: #{score[1]}"
-    prompt "Play again? (y or n)"
-    answer = gets.chomp.downcase
-    break unless answer.downcase == 'y'
-  end
 
+  prompt "Player: #{score[0]} - Computer: #{score[1]} - Ties: #{score[2]}"
+  prompt "Play again? (y or n)"
+  answer = gets.chomp.downcase
+  break unless answer == 'y'
+end
 
 prompt "Thanks for playing Tic Tac Toe! Good bye"
